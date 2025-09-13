5ab2d5f87d084417e50d3276ff7031643d40d06f
Jeron Cansanay <jeronscc@gmail.com>
2025-09-14 00:08:08 +0800

    math and debugger

---
+++ javascript/js/11math.js
@@ -0,0 +1,27 @@
+// Rounding Numbers
+
+const number = 3.6;
+
+const rounded = Math.round(number); // rounded to the nearest integer
+console.log(rounded);
+
+const ceil = Math.round(number); //round up: 4
+console.log(ceil);
+
+const floor = Math.floor(number); //round down: 3
+console.log(floor);
+
+
+// Exponential and logarithms
+
+const base = 2;
+const exponent = 3;
+
+console.log(Math.pow(base, exponent)); // power
+console.log(Math.sqrt(base)); // square root
+console.log(Math.log(base)); // natural logarithm
+
+// Random number generation
+
+const random = Math.random(); // provides random number between 0 and 1 inc decimal
+console.log(random);
