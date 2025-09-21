56cf20c22bb92adc0a299ed1e2268cbc0f660814
Jeron Cansanay <jeronscc@gmail.com>
2025-09-21 22:59:35 +0800

    try

--- nodejs/basics/try.js
+++ nodejs/basics/try.js
@@ -1,4 +1,21 @@
+class Person {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-for (let x = 0; x < 6; x++){
-    console.log(x)
+    sayName = () => {
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+const p1 = new Person("Jeron", 21);
+p1.sayName();
+
+console.log(p1.age);
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
 }
