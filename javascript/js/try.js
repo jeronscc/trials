9e6efbf4420165815bc3d11d5553306d8053f85b
Jeron Cansanay <jeronscc@gmail.com>
2025-09-28 21:23:41 +0800

    try

--- javascript/js/try.js
+++ javascript/js/try.js
@@ -1,6 +1,17 @@
 let arr = [1, 2, 3, 4, 5];
 
 for (let i in arr) {
-  console.log(i)
+  console.log(i);
+}
+
+
+for (let i of arr) {
+  console.log(i);
+}
+
+arr.forEach((i) => console.log(i))
+
+for (let i = 0; i < arr.length; i ++) {
+  console.log(i);
 }
 
