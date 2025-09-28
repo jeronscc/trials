6499263b11d1f41a81242e9692bc21ffa6b58de9
Jeron Cansanay <jeronscc@gmail.com>
2025-09-28 21:21:06 +0800

    try

--- javascript/js/try.js
+++ javascript/js/try.js
@@ -1,18 +1,6 @@
-// Define a JSON object representing information about a person
-const personJSON = {
-  "name": "John Doe",
-  "age": 30,
-  "email": "john@example.com",
-  "address": {
-    "street": "123 Main St",
-    "city": "Anytown",
-    "country": "USA"
-  },
-  "tags": ["JavaScript", "Node.js", "Web Development"],
-  "isStudent": false,
-  "workExperience": null
-};
-// Convert the JSON object to a string
-const jsonString = JSON.stringify(personJSON);
-console.log(personJSON)
-console.log(jsonString);
+let arr = [1, 2, 3, 4, 5];
+
+for (let i in arr) {
+  console.log(i)
+}
+
